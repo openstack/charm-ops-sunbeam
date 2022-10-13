6549c129a9cb6274616aefbb23acf9c34c52b843
James Page <james.page@canonical.com>
2022-10-13 10:06:19 +0100

    Use new rabbitmq-k8s operator and interfaces
    Use renamed rabbitmq-k8s operator in bundles and the new rabbitmq
    interface from the same operator in the codebase.
    
    This will require an update to fetch-libs.sh for all charms
    to rollout.
    
    Change-Id: I53c8b9d1194895bdf6322cf8a256f32459aa76d7

--- fetch-libs.sh
+++ fetch-libs.sh
@@ -8,7 +8,7 @@ charmcraft fetch-lib charms.nginx_ingress_integrator.v0.ingress
 charmcraft fetch-lib charms.data_platform_libs.v0.database_requires
 charmcraft fetch-lib charms.sunbeam_keystone_operator.v0.identity_service
 charmcraft fetch-lib charms.keystone_k8s.v0.cloud_credentials
-charmcraft fetch-lib charms.sunbeam_rabbitmq_operator.v0.amqp
+charmcraft fetch-lib charms.rabbitmq_k8s.v0.rabbitmq
 charmcraft fetch-lib charms.sunbeam_ovn_central_operator.v0.ovsdb
 charmcraft fetch-lib charms.observability_libs.v0.kubernetes_service_patch
 charmcraft fetch-lib charms.traefik_k8s.v1.ingress
